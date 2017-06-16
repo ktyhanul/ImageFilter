9e03956ab16ec16ba4611681f847e2b3a479624b
ktyhanul <ktyhanul@gmail.com>
2017-06-16 12:36:41 +0900

    add imagePicker

--- ImageFilter/ImageFilter/ViewController.swift
+++ ImageFilter/ImageFilter/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var filterButton: UIBarButtonItem!
     @IBOutlet weak var imageButton: UIBarButtonItem!
+    let imagePicker = UIImagePickerController()
     
     @IBAction func imageButtonAction(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
@@ -24,6 +27,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,7 +37,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
 
+    }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
